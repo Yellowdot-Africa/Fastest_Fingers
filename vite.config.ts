29288b2ffeea8f658d4ecbf725a2c8838c081b80
Blessing Peter <cherubpeters@gmail.com>
2024-08-19 00:23:54 +0100

    remove proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Assuming API requests are prefixed with '/api'
-      '/api': {
-        target: 'https://ydvassdp.com:2001',  // Target API server
-        changeOrigin: true,  // This sets the host header of the request to the target URL
-        rewrite: (path) => path.replace(/^\/api/, ''),  // Remove the /api prefix
-        secure: false,  // If your local dev is not using https, but the target does
-      }
-    }
-  }
+ 
 });
