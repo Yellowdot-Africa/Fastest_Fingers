8d3cba0ba45629400e2ba719dad400ae9f8ce45b
Mhizdhee <olaniyandorcas91@gmail.com>
2025-02-23 21:53:09 +0100

    fixes

--- src/pages/SubscriptionFailure.tsx
+++ src/pages/SubscriptionFailure.tsx
@@ -1,20 +1,26 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const SubscriptionFailure: React.FC  = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     navigate("/subscribe"); 
+  //   }, 5000); 
+  // }, [navigate]);
 
   useEffect(() => {
     setTimeout(() => {
-      navigate("/subscribe"); 
-    }, 5000); 
-  }, [navigate]);
+      window.location.href = "https://ydtelecel-sdp.netlify.app";
+    }, 8000);
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
       <h1 className="text-2xl font-bold text-red-400">Subscription Failed ❌</h1>
       <p className="mt-2 text-gray-700">Your subscription could not be completed.</p>
-      <p className="mt-2 text-gray-700">Redirecting you to the subscription page...</p>
+      <p className="mt-2 text-gray-700">Redirecting you to the landing page...</p>
     </div>
   );
 };
